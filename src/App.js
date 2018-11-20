6c225646a74f5eccab3c4c6624bfeedef5207b6a
Nathan Avilez <tungsteno22@gmail.com>
2018-11-19 20:33:40 -0600

    added mail and phone

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+import {
+  faStroopwafel,
+  faEnvelope,
+  faMobile,
+  faMobileAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 import './App.css';
 
-library.add(faStroopwafel, fab);
+library.add(faStroopwafel, fab, faEnvelope, faMobileAlt);
 
 class App extends Component {
   render() {
@@ -16,13 +21,21 @@ class App extends Component {
           <div>
             <h1>Nathan Avilez</h1>
             <h3>Software Engineer</h3>
-            <div>
+            <div id="contact">
               <a href="https://www.linkedin.com/in/nathanavilez/">
                 <FontAwesomeIcon icon={['fab', 'linkedin']} />
-              </a>
-              {'    '}
+              </a>{' '}
               <a href="https://github.com/tungsteno22">
                 <FontAwesomeIcon icon={['fab', 'github']} />
+              </a>{' '}
+              <a href="https://medium.com/@tungsteno22">
+                <FontAwesomeIcon icon={['fab', 'medium']} />
+              </a>{' '}
+              <a href="mailto:tungsteno22@gmail.com">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>{' '}
+              <a href="tel:+13109935831">
+                <FontAwesomeIcon icon={faMobileAlt} />
               </a>
             </div>
           </div>
