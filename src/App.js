c7521b4aaf30873bd3ff33ab4bd0339e386c4bfe
Nathan Avilez <tungsteno22@gmail.com>
2018-11-19 20:23:27 -0600

    added name and links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
+
+library.add(faStroopwafel, fab);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Nathan Avilez</h1>
+          <div>
+            <h1>Nathan Avilez</h1>
+            <h3>Software Engineer</h3>
+            <div>
+              <a href="https://www.linkedin.com/in/nathanavilez/">
+                <FontAwesomeIcon icon={['fab', 'linkedin']} />
+              </a>
+              {'    '}
+              <a href="https://github.com/tungsteno22">
+                <FontAwesomeIcon icon={['fab', 'github']} />
+              </a>
+            </div>
+          </div>
         </header>
       </div>
     );
