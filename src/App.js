8486649fc0e10809357d1dc231dd6fd6f824b96d
Nathan Avilez <tungsteno22@gmail.com>
2018-12-05 09:54:51 -0600

    added stack overflow and moved const to constants.js

--- src/App.js
+++ src/App.js
@@ -3,37 +3,11 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 import MediaLink from './MediaLink';
+import { mediaLinks } from './constants';
 
 import './App.css';
 
 library.add(fab, faEnvelope, faMobileAlt);
-
-const mediaLinks = [
-  {
-    href: 'https://www.linkedin.com/in/nathanavilez/',
-    icon: ['fab', 'linkedin'],
-  },
-  {
-    href: 'https://github.com/tungsteno22',
-    icon: ['fab', 'github'],
-  },
-  {
-    href: 'https://medium.com/@tungsteno22',
-    icon: ['fab', 'medium'],
-  },
-  {
-    href: 'https://angel.co/nathan-avilez',
-    icon: ['fab', 'angellist'],
-  },
-  {
-    href: 'mailto:tungsteno22@gmail.com',
-    icon: faEnvelope,
-  },
-  {
-    href: 'tel:+13109935831',
-    icon: faMobileAlt,
-  },
-];
 
 class App extends Component {
   render() {
---
+++ src/constants.js
@@ -0,0 +1,32 @@
+import { faEnvelope, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
+
+export const mediaLinks = [
+  {
+    href: 'https://www.linkedin.com/in/nathanavilez/',
+    icon: ['fab', 'linkedin'],
+  },
+  {
+    href: 'https://github.com/tungsteno22',
+    icon: ['fab', 'github'],
+  },
+  {
+    href: 'https://medium.com/@tungsteno22',
+    icon: ['fab', 'medium'],
+  },
+  {
+    href: 'https://stackoverflow.com/users/story/10746412',
+    icon: ['fab', 'stack-overflow'],
+  },
+  {
+    href: 'https://angel.co/nathan-avilez',
+    icon: ['fab', 'angellist'],
+  },
+  {
+    href: 'mailto:tungsteno22@gmail.com',
+    icon: faEnvelope,
+  },
+  {
+    href: 'tel:+13109935831',
+    icon: faMobileAlt,
+  },
+];
